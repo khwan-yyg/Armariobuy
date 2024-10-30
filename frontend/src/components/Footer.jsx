e72160039fa882a67bb2310c6758a485fc022132
khwankamon <khwan_yyg@outlook.com>
2024-10-30 15:03:09 +0700

    style: add color text in footer page

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,9 +3,9 @@ import { assets } from "../assets/assets";
 
 const Footer = () => {
   return (
-    <div className="">
+    <div>
       <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
-        <div className="">
+        <div>
           <img src={assets.logo} className="mb-5 w-32" alt="" />
           <p className="w-full md:w-3/2 text-gray-600">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam et
@@ -14,17 +14,18 @@ const Footer = () => {
             mollitia voluptatum quam!
           </p>
         </div>
-        <div className="">
+
+        <div>
           <p className="text-xl font-medium mb-5">COMPANY</p>
           <ul className="flex flex-col gap-1 text-gray-600">
-            <li className="">Home</li>
-            <li className="">About us</li>
-            <li className="">Delivery</li>
-            <li className="">Privacy policy</li>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
           </ul>
         </div>
 
-        <div className="">
+        <div>
           <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
           <ul className="flex flex-col gap-1 text-gray-600">
             <li>+1234567890</li>
@@ -33,7 +34,7 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="">
+      <div>
         <hr />
         <p className="py-5 text-sm text-center">
           Copyright 2024@ - All Right Reserved.
