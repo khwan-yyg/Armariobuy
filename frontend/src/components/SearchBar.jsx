2e289d244cf8a01c60d764312eaa39262659736d
khwankamon <khwan_yyg@outlook.com>
2024-10-31 16:00:36 +0700

    style(searchbar): fix z-index overlay with menu mobile screen

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -21,7 +21,7 @@ const SearchBar = () => {
 
   // showSearch is toggleicon and visible is show in path collection
   return showSearch && visible ? (
-    <div className="border-t border-b bg-gray-50 text-center top-20 left-0 fixed w-full z-50 ">
+    <div className="border-t border-b bg-gray-50 text-center top-20 left-0 fixed w-full z-40 ">
       <div className="inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2">
         <input
           value={search}
