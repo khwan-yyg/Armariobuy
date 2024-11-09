dceeea43ae596c138bf7a3744874a59d10d2a029
khwankamon <khwan_yyg@outlook.com>
2024-11-09 12:02:35 +0700

    feat(admin): ui admin dashbord

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { assets } from "../assets/assets";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex items-center py-2 px-[4%] justify-between border-b sticky top-0 left-0 z-50 bg-gray-50">
+      <img className="w-[max(10%,80px)]" src={assets.logo} alt="" />
+      <button className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm">
+        Logout
+      </button>
+    </div>
+  );
 };
 
 export default Navbar;
