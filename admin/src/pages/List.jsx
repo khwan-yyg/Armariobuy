4980f13205c6d64183881d302ff80dc34dbe219a
khwankamon <khwan_yyg@outlook.com>
2024-11-10 12:19:18 +0700

    style(admin): ui list order add page

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { assets } from "../assets/assets";
 
 const List = () => {
-  return <div>List</div>;
+  return (
+    <div>
+      <p className="mb-2">All Products List</p>
+
+      <div className="flex flex-col gap-2">
+        <div className="hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center py-1 px-2 border bg-gray-100 text-sm">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b className="text-center">Action</b>
+        </div>
+
+        <div className="grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-sm">
+          <img className="w-12" src={assets.upload_area} alt="" />
+          <p>Kid Tapered Slim Fit Trouser</p>
+          <p>Kids</p>
+          <p>$38</p>
+          <p className="text-right md:text-center cursor-pointer text-lg">X</p>
+        </div>
+
+        <div className="grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-sm">
+          <img className="w-12" src={assets.order_icon} alt="" />
+          <p>Kid Tapered Slim Fit Trouser</p>
+          <p>Kids</p>
+          <p>$38</p>
+          <p className="text-right md:text-center cursor-pointer text-lg">X</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default List;
--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -1,7 +1,49 @@
 import React from "react";
+import { assets } from "../assets/assets";
 
 const Orders = () => {
-  return <div>Orders</div>;
+  return (
+    <div>
+      <h3>Order Page</h3>
+      <div>
+        <div className="grid grid-cols-1 sm:grid-cols-[0.5fr_2fr_1fr] lg:grid-cols-[0.5fr_2fr_1fr_1fr_1fr] gap-3 items-start border-2 border-gray-200 p-5 md:p-8 my-3 md:my-4 text-xs sm:text-sm text-gray-700">
+          <img className="w-12" src={assets.parcel_icon} alt="" />
+
+          <div>
+            <div>
+              <p className="py-0.5">
+                Kid Tapered Slim Fit Trouser x 1 <span> XL </span>
+              </p>
+            </div>
+
+            <p className="mt-3 mb-2 font-medium">name customer</p>
+
+            <div>
+              <p>Adress</p>
+              <p>Adress, Phan Chiang rai, Thai, 57120</p>
+            </div>
+          </div>
+
+          <div>
+            <p className="text-sm sm:text-[15px]">Items : 1</p>
+            <p className="mt-3">Method : COD</p>
+            <p>Payment : Pending</p>
+            <p>Date : 11/9/2024</p>
+          </div>
+
+          <p className="text-sm sm:text-[15px]">$50</p>
+
+          <select className="p-2 font-semibold">
+            <option value="Order Placed">Order Placed</option>
+            <option value="Packing">Packing</option>
+            <option value="Shipped">Shipped</option>
+            <option value="Out for delivery">Out for delivery</option>
+            <option value="Delivered">Delivered</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
